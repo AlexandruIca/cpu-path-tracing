1ff00f6d4e16df86b395aa9784b28403ddd64c05
Alexandru Ica <alexandruica703@gmail.com>
2021-03-04 13:13:28 +0200

    Use guix for release builds

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 
-CC=gcc CXX=g++ cmake -G"Ninja" \
-    -DCMAKE_BUILD_TYPE=Release \
-    ..
-
-cmake --build .
+guix environment -m ../scripts/gcc-latest.scm -- ../scripts/release-build.sh
