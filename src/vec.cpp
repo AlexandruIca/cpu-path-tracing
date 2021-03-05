ed68fd7aa139114ae69850912d6942d9378eb923
Alexandru Ica <alexandruica703@gmail.com>
2021-03-05 21:06:41 +0200

    Add an index method for `vec3`

--- src/vec.cpp
+++ src/vec.cpp
@@ -1,5 +1,6 @@
 #include "vec.hpp"
 
+#include <cassert>
 #include <cmath>
 
 namespace pt {
@@ -44,6 +45,22 @@ auto vec3::dot(vec3 const& b) const noexcept -> double
 auto vec3::cross(vec3 const& b) const noexcept -> vec3
 {
     return vec3{ y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x };
+}
+
+auto vec3::operator[](int const index) const noexcept -> double const&
+{
+    switch(index) {
+    case 0:
+        return x;
+    case 1:
+        return y;
+    case 2:
+        return z;
+    default:
+        assert(false && "Maimum index for `vec3::operator[]` is 2");
+    }
+
+    return x;
 }
 
 } // namespace pt
